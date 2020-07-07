4f7b58aaa30569f6569653d3540f3efa3b6c9541
NickoDante <nickodante24@gmail.com>
2020-07-06 20:32:45 -0500

    ** Video 70 & 71 : Introduction to UI & Creating MainMenuMap

---
+++ Source/Arkde_RoomPuzzle/Public/Core/RP_PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "RP_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKDE_ROOMPUZZLE_API ARP_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
