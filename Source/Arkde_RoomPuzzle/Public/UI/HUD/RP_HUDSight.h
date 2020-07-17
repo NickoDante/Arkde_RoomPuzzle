b9085078b41b013f94f7abcb61c2988dc40db133
NickoDante <nickodante24@gmail.com>
2020-07-16 19:55:48 -0500

    ** Video 77 : Preparing HUD.

---
+++ Source/Arkde_RoomPuzzle/Public/UI/HUD/RP_HUDSight.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RP_HUDSight.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKDE_ROOMPUZZLE_API URP_HUDSight : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
